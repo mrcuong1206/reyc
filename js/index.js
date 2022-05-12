7108de808b5f11e02e53ec599e11572114cc5899
mrcuong1206 <mrmanhcuong126@gmail.com>
2022-05-12 15:33:30 +0700

    initialcommit

---
+++ js/index.js
@@ -0,0 +1,29 @@
+// Menu Bar
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active');
+}
+window.onscroll = () => {
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+
+    // Back to top
+    if (window.scrollY > 60) {
+        document.querySelector('#scroll-top').classList.add('active');
+    } else {
+        document.querySelector('#scroll-top').classList.remove('active');
+    }
+}
+
+// Loader
+function loader() {
+    document.querySelector('.loader-container').classList.add('fade-out');
+}
+
+function fadeOut() {
+    setInterval(loader, 3000);
+}
+window.onload = fadeOut();
